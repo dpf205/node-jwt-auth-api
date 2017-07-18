64b3010e0fbc13ecd9a7ad7914da0d54fbf3fe6f
dpf205 <dpf205@gmail.com>
2017-07-18 16:51:53 -0400

    added some mongodb library  examples

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -3,32 +3,32 @@
 // const MongoClient = require('mongodb').MongoClient;
 
 MongoClient.connect('mongodb://localhost:27017/TaskList', (err, db) => {
-    if (err) {
-        return console.log('unable to connect to  mongodb server');
-    }
-    console.log('connected to mongodb server');
+	if (err) {
+		return console.log('unable to connect to  mongodb server');
+	}
+	console.log('connected to mongodb server');
 
-    // db.collection('Tasks').insertOne({
-    // 	text: "something to do",
-    // 	completed: false
-    // }, (err, result) => {
-    // 	if (err) {
-    // 		return console.log('unable to insert task', err)
-    // 	}
-    // 	console.log(JSON.stringify(result.ops, undefined, 2)); //ops attribute stores all the docs that are inserted
-    // });
+	db.collection('Tasks').insertOne({
+		text: "something to do",
+		completed: false
+	}, (err, result) => {
+		if (err) {
+			return console.log('unable to insert task', err)
+		}
+		console.log(JSON.stringify(result.ops, undefined, 2)); //ops attribute stores all the docs that are inserted
+	});
 
-    // db.collection('Users').insertOne({
-    // 	name: 'Peter',
-    // 	age: 35,
-    // 	location: 'Pittsburgh'
-    // }, (err, result) => {
-    // 	if (err) {
-    // 		return console.log('unable to insert user data', err);
-    // 	}
-    // 	console.log(JSON.stringify(result.ops, undefined, 2)); // ops attribute stores all the docs that are inserted
-    // 	console.log(JSON.stringify(result.ops[0]._id.getTimestamp())); // use defualt ObjectId to get the timestamp
-    // });Elizabeth Holmes
+	db.collection('Users').insertOne({
+		name: 'Peter',
+		age: 35,
+		location: 'Pittsburgh'
+	}, (err, result) => {
+		if (err) {
+			return console.log('unable to insert user data', err);
+		}
+		console.log(JSON.stringify(result.ops, undefined, 2)); // ops attribute stores all the docs that are inserted
+		console.log(JSON.stringify(result.ops[0]._id.getTimestamp())); // use defualt ObjectId to get the timestamp
+	});
 
-    db.close();
+	// db.close();
 });
---
+++ playground/mongodb-delete.js
@@ -0,0 +1,38 @@
+const {
+	MongoClient,
+	ObjectId
+} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TaskList', (err, db) => {
+
+	db.collection('Tasks').deleteMany({
+		text: "finish api design specification"
+	}).then((result) => {
+		console.log(result)
+	});
+
+	db.collection('Tasks').deleteOne({
+		text: "walk the dog"
+	}).then((result) => {
+		console.log(result);
+	});
+
+	db.collection('Tasks').findOneAndDelete({
+		completed: false
+	}).then((result) => {
+		console.log(result);
+	});
+
+	db.collection('Users').deleteMany({
+		name: 'John'
+	}).then((result) => {
+		console.log(result);
+	});
+
+	db.collection('Users').deleteOne({
+		_id: ObjectId("596a2fd8a161d30daa4d19df")
+	}, (result) => {
+		console.log(result);
+	});
+	// db.close();
+});
