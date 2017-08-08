42655cebcd0ba64c3ed1202def6e27fb22a6771f
dpf205 <dpf205@gmail.com>
2017-08-08 16:08:52 -0400

    modified mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,11 +6,11 @@ require('dotenv').config(); // https://github.com/motdotla/dotenv
 
 var mongoose = require('mongoose'); // @4.5.9 to avoid current verbose warning triggered by later versions
 
-const mLab_URI = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds127883.mlab.com:27883/node-tasks-api`;
+const MLAB_URI = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds127883.mlab.com:27883/node-tasks-api`;
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(mLab_URI, () =>{
+mongoose.connect(MLAB_URI, () =>{
 	console.log('** connected to mLab URI\n');
 });
 
