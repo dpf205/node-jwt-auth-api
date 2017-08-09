a1b2cf53b31a8623098e9f6b55e82cbc6b3ba726
dpf205 <dpf205@gmail.com>
2017-08-09 19:39:59 -0400

    refactoring db/mongoose.js console.logs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -8,7 +8,7 @@ var mongoose = require('mongoose'); // @4.5.9 to avoid current verbose warning t
 
 mongoose.Promise = global.Promise;
 
-// connect to Heroku addon mLab db "MONGOLAB_BLUE_URI" or directly via mlab account MLAB_URI
+// connect to Heroku addon mLab db "MONGOLAB_BLUE_URI" or directly via mlab account "MLAB_URI"
 mongoose.connect(process.env.MONGOLAB_BLUE_URI || process.env.MLAB_URI, () => {
 	if(process.env.MLAB_URI){
 		console.log('** connected to private mLab DB instance');
